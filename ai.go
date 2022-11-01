4ddce7b9153efd21504c0e77bf2323926ee6d5b5
Gavri Kepets <gkepets@gmail.com>
2022-11-01 15:54:16 -0400

    fixed ai timing.

--- ai.go
+++ ai.go
@@ -2,98 +2,84 @@ package main
 
 import (
 	"fmt"
-	// "time"
+	"time"
 	"math"
+	"math/rand"
 )
 
 func getAITurn(board[8][8] int, legalMoves []int, turn int, timeLimit int) int{
-	fmt.Printf("Getting AI turn. There are %d available moves.\n", len(legalMoves))
-	// LOOK INTO REORDERING MOVES FROM BEST TO WORSE TO INCREASE PRUNING PERFORMANCE
-	depth := 2
-	var scores []int
-
-	for _, move := range legalMoves {
-		fmt.Printf("Starting move %s.\n", convertIntToCoords(move))
-		var p = []int{move}
-		tmpBoard := placePiece(board, move, turn)
-		tmpTurn := turn
-		if turn == 1 {
-			tmpTurn = 2
+	fmt.Printf("Getting AI turn.\nThere are %d available moves: ", len(legalMoves))
+	fmt.Printf("[")
+	for i, move := range legalMoves {
+		if i == len(legalMoves) - 1 {
+			fmt.Printf("(%s)", convertIntToCoords(move))
 		} else {
-			tmpTurn = 1
+			fmt.Printf("(%s), ", convertIntToCoords(move))
 		}
-		score := minimax(tmpBoard, depth, math.MinInt, math.MaxInt, turn, tmpTurn, p)
-		fmt.Printf("Final score: %d\n", score)
-		scores = append(scores, score)
 	}
-	
+	fmt.Printf("]\n")
+
+	start := time.Now()
+
+	if len(legalMoves) == 1 { // if there is only one available move
+		return 0
+	}
+
+	depth := 0
 	bestI := 0
+	scores := make([]int, len(legalMoves))
+	depths := make([]int, len(legalMoves))
+	cutOff := false
+
+	out:
+	for {
+		if float64(timeLimit) - time.Since(start).Seconds() < float64(timeLimit)/2.0 {
+			break out
+		}
+
+		for i, move := range legalMoves {
+			if float64(timeLimit) - time.Since(start).Seconds() < 0.25 {
+				cutOff = true
+				break out
+			}
+
+			var p = []int{move}
+			tmpBoard := placePiece(board, move, turn)
+			tmpTurn := turn
+			if turn == 1 {
+				tmpTurn = 2
+			} else {
+				tmpTurn = 1
+			}
+
+			score := minimax(tmpBoard, depth, math.MinInt, math.MaxInt, turn, tmpTurn, p)
+			scores[i] = score
+			depths[i] = depth
+		}
+
+		depth++
+	}
+
 	bestScore := math.MinInt
 	for i, score := range(scores){
-		if score >= bestScore {
+		if score > bestScore {
+			bestScore = score
+			bestI = i
+		} else if score == bestScore && rand.Float64() > 0.5{
 			bestScore = score
 			bestI = i
 		}
 	}
+	
+	fmt.Printf("Scores evaluated to be %v. Finished in %f seconds.\n", scores, time.Since(start).Seconds())
+
+	if cutOff {
+		fmt.Printf("Cut off at depth %d. Turn selected from depth %d", depth, depths[bestI])
+	} else {
+		fmt.Printf("Successfully evaluated until depth %d.", depth)
+	}
 
 	return bestI
-	// start := time.Now()
-
-	// if len(legalMoves) == 1 { // if there is only one available move
-	// 	return 0
-	// }
-	
-	// bestMove := 0
-	// depth := 1
-	// completeDepth := true
-	// // fmt.Printf("checking %d different turns\n", len(legalMoves))
-
-	// for {
-	// 	if float64(timeLimit) - time.Since(start).Seconds() < float64(timeLimit)/2.0 {
-	// 		break
-	// 	}
-
-	// 	value := math.MinInt
-	// 	bestMoveDepth := 0
-	// 	// fmt.Printf("Depth: %d, found %d moves\n", depth, len(legalMoves))
-
-	// 	for i, move := range legalMoves {
-	// 		// fmt.Printf("Placing move on board: %d\n", move)
-	// 		tmpBoard := placePiece(board, move, turn)
-	// 		tmpTurn := turn
-
-	// 		if turn == 1 {
-	// 			tmpTurn = 2
-	// 		} else {
-	// 			tmpTurn = 1
-	// 		}
-
-	// 		score := alphaBeta(tmpBoard, math.MinInt, math.MaxInt, depth, tmpTurn, turn)
-	// 		// fmt.Printf("Score: %d\n", score)
-
-	// 		if score > value {
-	// 			value = score
-	// 			bestMoveDepth = i
-	// 		}
-
-	// 		if time.Since(start).Seconds() > float64(timeLimit) {
-	// 			completeDepth = false
-	// 			break
-	// 		}
-	// 	}
-
-	// 	if !completeDepth {
-	// 		fmt.Printf("AI was interrupted at depth %d.\n", depth)
-	// 		break
-	// 	}
-
-	// 	bestMove = bestMoveDepth
-	// 	depth++
-	// }
-
-	// fmt.Printf("AI reached a depth of %d after %f seconds.\n", depth, time.Since(start).Seconds())
-	
-	// return bestMove
 }
 
 func minimax(board[8][8] int, depth int, alpha int, beta int, maxTurn int, currentTurn int, previousMoves []int) (int){
@@ -164,14 +150,14 @@ func minimax(board[8][8] int, depth int, alpha int, beta int, maxTurn int, curre
 // given a board and a player, return a heuristic score for the player
 func calculateHeuristicScore(board [8][8] int, player int) int{
 	scoreBoard := [8][8]int{
-		{200, -10, 10, 10, 10, 10, -10, 200},
+		{200, -10, 15, 10, 10, 15, -10, 200},
 		{-10, -10, -1, -1, -1, -1, -1, -10},
+		{15, -1, 1, 1, 1, 1, -1, 15},
 		{10, -1, 1, 1, 1, 1, -1, 10},
 		{10, -1, 1, 1, 1, 1, -1, 10},
-		{10, -1, 1, 1, 1, 1, -1, 10},
-		{10, -1, 1, 1, 1, 1, -1, 10},
+		{15, -1, 1, 1, 1, 1, -1, 15},
 		{-10, -10, -1, -1, -1, -1, -10, -10},
-		{200, -10, 10, 10, 10, 10, -10, 200}}
+		{200, -10, 15, 10, 10, 15, -10, 200}}
 
 	score := 0
 	for i := 0; i < 8; i++ {
@@ -181,6 +167,8 @@ func calculateHeuristicScore(board [8][8] int, player int) int{
 			}
 		}
 	}
+
+
 
 	return score
 }
--- ai2.go
+++ ai2.go
@@ -1,71 +1,67 @@
 package main
 
-import (
-	"math"
-	// "fmt"
-)
 
-func minimax(board[8][8] int, depth int, alpha int, beta int, maxTurn int, currentTurn int, previousMoves []int) (int){
-	if depth == 0 {
-		score := calculateHeuristicScore(board, maxTurn)
-		// fmt.Printf("Best heuristic had a score of %d.\n", score)
-		// fmt.Println(previousMoves)
-		return score
-	}
+// func minimax(board[8][8] int, depth int, alpha int, beta int, maxTurn int, currentTurn int, previousMoves []int) (int){
+// 	if depth == 0 {
+// 		score := calculateHeuristicScore(board, maxTurn)
+// 		// fmt.Printf("Best heuristic had a score of %d.\n", score)
+// 		// fmt.Println(previousMoves)
+// 		return score
+// 	}
 
-	if maxTurn == currentTurn {
-		// fmt.Println("Max turn.")
-		maxVal := math.MinInt
-		legalMoves := getAllLegalMoves(currentTurn, board)
+// 	if maxTurn == currentTurn {
+// 		// fmt.Println("Max turn.")
+// 		maxVal := math.MinInt
+// 		legalMoves := getAllLegalMoves(currentTurn, board)
 
-		for _, move := range(legalMoves) {
-			// fmt.Printf("Checking turn %s\n", convertIntToCoords(move))
-			tmpBoard := placePiece(board, move, currentTurn)
-			tmpPreviousMoves := append(previousMoves, move)
-			tmpTurn := currentTurn
+// 		for _, move := range(legalMoves) {
+// 			// fmt.Printf("Checking turn %s\n", convertIntToCoords(move))
+// 			tmpBoard := placePiece(board, move, currentTurn)
+// 			tmpPreviousMoves := append(previousMoves, move)
+// 			tmpTurn := currentTurn
 
-			if tmpTurn == 1 {
-				tmpTurn = 2
-			} else {
-				tmpTurn = 1
-			}
+// 			if tmpTurn == 1 {
+// 				tmpTurn = 2
+// 			} else {
+// 				tmpTurn = 1
+// 			}
 
-			val := minimax(tmpBoard, depth - 1, alpha, beta, maxTurn, tmpTurn, tmpPreviousMoves)
-			// fmt.Printf("found val %d\n", val)
-			maxVal = maximum(maxVal, val)
-			alpha = maximum(alpha, val)
+// 			val := minimax(tmpBoard, depth - 1, alpha, beta, maxTurn, tmpTurn, tmpPreviousMoves)
+// 			// fmt.Printf("found val %d\n", val)
+// 			maxVal = maximum(maxVal, val)
+// 			alpha = maximum(alpha, val)
 
-			if beta <= alpha {
-				break
-			}
-		}
-		return maxVal
-	} else {
-		// fmt.Println("Min turn.")
-		minVal := math.MaxInt
-		legalMoves := getAllLegalMoves(currentTurn, board)
+// 			if beta <= alpha {
+// 				break
+// 			}
+// 		}
+// 		return maxVal
+// 	} else {
+// 		// fmt.Println("Min turn.")
+// 		minVal := math.MaxInt
+// 		legalMoves := getAllLegalMoves(currentTurn, board)
 
-		for _, move := range(legalMoves) {
-			// fmt.Printf("Checking turn %s\n", convertIntToCoords(move))
-			tmpBoard := placePiece(board, move, currentTurn)
-			tmpPreviousMoves := append(previousMoves, move)
-			tmpTurn := currentTurn
+// 		for _, move := range(legalMoves) {
+// 			// fmt.Printf("Checking turn %s\n", convertIntToCoords(move))
+// 			tmpBoard := placePiece(board, move, currentTurn)
+// 			tmpPreviousMoves := append(previousMoves, move)
+// 			tmpTurn := currentTurn
 
-			if tmpTurn == 1 {
-				tmpTurn = 2
-			} else {
-				tmpTurn = 1
-			}
+// 			if tmpTurn == 1 {
+// 				tmpTurn = 2
+// 			} else {
+// 				tmpTurn = 1
+// 			}
 
-			val := minimax(tmpBoard, depth - 1, alpha, beta, maxTurn, tmpTurn, tmpPreviousMoves)
-			// fmt.Printf("found val %d\n", val)
-			minVal = minimum(minVal, val)
-			beta = minimum(beta, val)
+// 			val := minimax(tmpBoard, depth - 1, alpha, beta, maxTurn, tmpTurn, tmpPreviousMoves)
+// 			// fmt.Printf("found val %d\n", val)
+// 			minVal = minimum(minVal, val)
+// 			beta = minimum(beta, val)
 
-			if beta <= alpha {
-				break
-			}
-		}
-		return minVal
-	}
-}
+// 			if beta <= alpha {
+// 				break
+// 			}
+// 		}
+// 		return minVal
+// 	}
+// }
